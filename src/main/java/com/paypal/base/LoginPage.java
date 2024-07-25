f71539e17c837d8538d0bae560c762ac3b00620e
Sylvester <ssylvester281@gmail.com>
2024-07-25 13:47:23 -0400

    Added login functionality to the LoginPage class

--- src/main/java/com/paypal/base/LoginPage.java
+++ src/main/java/com/paypal/base/LoginPage.java
@@ -1,4 +1,27 @@
 package com.paypal.base;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.Arrays;
+
+public class LoginPage extends WebPage {
+    @FindBy(id = "login")
+    protected WebElement usernameField;
+    @FindBy(id = "password")
+    protected WebElement passwordField;
+    @FindBy(name = "commit")
+    protected WebElement loginButton;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void doLogin(String username, String password) {
+        waitForVisibilityOfElements(Arrays.asList(usernameField, passwordField, loginButton));
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+        loginButton.click();
+    }
 }
